b240dcc93bd4d4ad755106b0e90f5a31f15e1b30
Tong Zhao <tongzhao997@gmail.com>
2025-06-19 02:07:34 -0400

    update with modern settings

--- download-latest.sh
+++ download-latest.sh
@@ -1,1 +1,1 @@
-yt-dlp --config-locations conf.conf https://www.youtube.com/playlist?list=PLvswIqZLpR-VpoMuxzO9oT3F9-FtHScpn
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/playlist?list=PLvswIqZLpR-VpoMuxzO9oT3F9-FtHScpn
