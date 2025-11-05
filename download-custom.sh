5dcc3318ee33fe03b13b7fc43600c5677a10ce52
Tong Zhao <tongzhao997@gmail.com>
2025-11-05 10:49:49 -0500

    update ytdl and conf

---
+++ download-custom.sh
@@ -0,0 +1,21 @@
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=3tM9SoJ-MpU
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=zK2iJEiW56Q
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=VDmXrdYQ6kg
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=zjbq8J3uZUI
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=0HklBNtziNQ
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=tlxf4HhIE_A
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=wdYi8IuEz3U
+
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=IRafdd39bQ8
+
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=3bDfowR6U0c
+
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=KyPjE1Sn-Ts
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=2F3y2bJzDJQ
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=riyVcdAOOC4
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=tIWJFDHQL2I
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=SrniBr3QPqM
+
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=NMqamER6npU
+./yt-dlp_linux --cookies-from-browser chrome --config-locations conf.conf https://www.youtube.com/watch?v=j9x69-vlcic
+
